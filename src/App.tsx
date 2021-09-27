897937ff3cee73f0c4916db6ad5602fb31577f3d
inmikhaylov <ilya.mihailov.dev@gmail.com>
2021-09-27 21:03:04 +0300

    Added some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import myPhoto from './images/MainPhoto.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lol kek cheburek</h1>
+        <header className="App-header">
+        <img src={myPhoto} className="Main-photo" alt="photo" />
+          <p>
+              That's my site.
+          </p>
       </header>
     </div>
   );
